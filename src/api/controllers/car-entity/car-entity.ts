8fa33939714814c81b68d7a302ff88cbf54f64f7
Michael Chan <mckchan13@gmail.com>
2022-07-18 20:23:16 -0700

    configured get route to return all listings, refactored the get and post route to return a message and the specified resource

--- src/api/controllers/car-entity/car-entity.ts
+++ src/api/controllers/car-entity/car-entity.ts
@@ -4,19 +4,29 @@ import { VINDecoderMiddleware } from "../../middlewares";
 import { Car, Listing } from "../../models";
 
 type entityPropertyOmissions = "id" | "listing" | "car" | "createdDate" | "updatedDate" | "deletedDate";
+
 type listingInputOmissions = "car" | "createdDate" | "updatedDate" | "deletedDate";
+
+interface returnResource<T> {
+  message: string;
+  resource: T
+}
 
 @JsonController("/car")
 @UseBefore(json())
 export class CarController {
   @Get("/listings")
-  get(): string {
-    return "Returns all car listings";
+  async get(): Promise<returnResource<Listing[]>> {
+    const allListings = await Listing.find();
+    return {
+      message: "All listings found.",
+      resource: allListings
+    };
   }
 
   @Post("/create-listing")
   @UseBefore(VINDecoderMiddleware)
-  async create(@Body() body: Omit<Listing & Car, entityPropertyOmissions>): Promise<string> {
+  async create(@Body() body: Omit<Listing & Car, entityPropertyOmissions>): Promise<returnResource<Listing>> {
     const { vin } = body;
     const existingCar = await Car.find({
       where: {
@@ -27,7 +37,10 @@ export class CarController {
     const car = carIsFound ? existingCar[0] : await Car.create(body).save();
     const newListing = await Listing.create({...body, car }).save();
     const { id: listingId } = newListing;
-    return `Posted new listing to database. Created Listing id: ${listingId}`;
+    return {
+      message:`Posted new listing to database. Created Listing id: ${listingId}`,
+      resource: newListing
+    };
   }
 
   @Put("/update-listing")
@@ -44,24 +57,3 @@ export class CarController {
     return `Deleted listing from database. Deleted Listing id: ${listingId}`;
   }
 }
-
-// 97449fbf-d1e7-45ea-be98-900952acaa20
-
-// const { vin, make, model, year } = query;
-// const car = new Car();
-// car.vin = vin;
-// car.make = make;
-// car.model = model;
-// car.year = +year;
-// await car.save();
-
-// const { licenseNumber, registrationName, registrationState, registrationExpirationDate, carValue, currentMileage } = body;
-// const listing = new Listing();
-// listing.licenseNumber = licenseNumber;
-// listing.registrationName = registrationName;
-// listing.registrationState = registrationState;
-// listing.registrationExpirationDate = registrationExpirationDate;
-// listing.carValue = carValue;
-// listing.currentMileage = currentMileage;
-// listing.car = car;
-// await listing.save();

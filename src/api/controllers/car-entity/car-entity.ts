05a573a67ba569f934500d0bb673f18c9b6781ab
Michael Chan <mckchan13@gmail.com>
2022-07-18 19:25:15 -0700

    configured put and delete routes, updated post route to check for existing car in database before creating a new car

--- src/api/controllers/car-entity/car-entity.ts
+++ src/api/controllers/car-entity/car-entity.ts
@@ -3,8 +3,11 @@ import { Body, Get, Put, Delete, JsonController, Post, UseBefore } from "routing
 import { VINDecoderMiddleware } from "../../middlewares";
 import { Car, Listing } from "../../models";
 
+type entityPropertyOmissions = "id" | "listing" | "car" | "createdDate" | "updatedDate" | "deletedDate";
+type listingInputOmissions = "car" | "createdDate" | "updatedDate" | "deletedDate";
+
 @JsonController("/car")
-@UseBefore(json(), VINDecoderMiddleware)
+@UseBefore(json())
 export class CarController {
   @Get("/listings")
   get(): string {
@@ -12,23 +15,37 @@ export class CarController {
   }
 
   @Post("/create-listing")
-  async create(@Body() body: Omit<Listing & Car, "id" | "date" | "listing" | "car">): Promise<string> {
-    const car = await Car.create(body).save();
-    await Listing.create({...body, car }).save();
-    return "Posted new listing to database.";
+  @UseBefore(VINDecoderMiddleware)
+  async create(@Body() body: Omit<Listing & Car, entityPropertyOmissions>): Promise<string> {
+    const { vin } = body;
+    const existingCar = await Car.find({
+      where: {
+        vin,
+      }
+    });
+    const carIsFound = !!existingCar.length;
+    const car = carIsFound ? existingCar[0] : await Car.create(body).save();
+    const newListing = await Listing.create({...body, car }).save();
+    const { id: listingId } = newListing;
+    return `Posted new listing to database. Created Listing id: ${listingId}`;
   }
 
   @Put("/update-listing")
-  async update(@Body() body: Omit<Listing & Car, "id" | "date" | "listing" | "car">): Promise<string> {
-    return "Updated listing to database.";
+  async update(@Body() body: Omit<Listing, listingInputOmissions>): Promise<string> {
+    const { id: listingId } = body;
+    await Listing.update(listingId, body);
+    return `Updated listing to database. Updated Listing id: ${listingId}`;
   }
 
   @Delete("/delete-listing")
-  async delete(@Body() body: Omit<Listing & Car, "id" | "date" | "listing" | "car">): Promise<string> {
-    return "Deleted listing from database.";
+  async delete(@Body() body: Omit<Listing, listingInputOmissions>): Promise<string> {
+    const { id: listingId } = body;
+    await Listing.delete(listingId);
+    return `Deleted listing from database. Deleted Listing id: ${listingId}`;
   }
 }
 
+// 97449fbf-d1e7-45ea-be98-900952acaa20
 
 // const { vin, make, model, year } = query;
 // const car = new Car();
